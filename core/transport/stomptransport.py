2d51d55eb29c00a6075b0a5943de5c72275440db
muhammad.aqshol <muhammad.aqshol@usg.co.id>
2022-02-03 13:30:22 +0700

    Adding STOMP MQ Transport Protocol

---
+++ core/transport/stomptransport.py
@@ -0,0 +1,66 @@
+#
+# Copyright (c) 2019 Busana Apparel Group. All rights reserved.
+#
+# This product and it's source code is protected by patents, copyright laws and
+# international copyright treaties, as well as other intellectual property
+# laws and treaties. The product is licensed, not sold.
+#
+# The source code and sample programs in this package or parts hereof
+# as well as the documentation shall not be copied, modified or redistributed
+# without permission, explicit or implied, of the author.
+#
+from core.transhandler import TransportHandler
+from stompest.config import StompConfig
+from stompest.protocol.spec import StompSpec
+from stompest.sync import Stomp
+import logging
+import time
+
+class StompTransport(TransportHandler):
+
+    def __init__(self):
+        self.heartbeat = 20000
+        self.stomp_config = None
+        self.CLIENT_ID = "cntbridge01"
+        super(StompTransport, self).__init__()
+
+    def do_configure(self):
+        super(StompTransport, self).do_configure()
+
+    def do_listen(self):
+        if not self.stomp_config:
+            self.stomp_config = StompConfig("tcp://{0}:{1}".format(self.get_transport_address(),
+                                                                   self.get_transport_port()),
+                                                                   login = self.get_transport_user(),
+                                                                   passcode = self.get_transport_password(),
+                                                                   version = StompSpec.VERSION_1_2)
+        client = Stomp(self.stomp_config)
+        logging.info("Subscribing {} on channel {}".format(self.get_transport_address(),self.get_transport_channel()))
+        client.connect(versions=[StompSpec.VERSION_1_2], heartBeats=(self.heartbeat, self.heartbeat))
+        client_heartbeat = client.clientHeartBeat / 1000.0
+        token = client.subscribe(self.get_transport_channel(), {StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL,
+                                                                  StompSpec.ID_HEADER: self.CLIENT_ID})
+        try:
+            try:
+                while True:
+                    if client.canRead(0):
+                        frame = client.receiveFrame()
+                        cmd_str = frame.body
+                        client.ack(frame)
+                        if cmd_str is not None:
+                            self.handle_message(cmd_str)
+                        else:
+                            logging.info("Message is Empty, bypassing")
+                    else:
+                        time.sleep(0.4)
+                    if (time.time() - client.lastSent) > client_heartbeat:
+                        client.beat()
+                    if not self.started:
+                        client.unsubscribe(token)
+                        break
+            except Exception as ex:
+                logging.error(ex)
+                client.unsubscribe(token)
+                raise
+        finally:
+            client.disconnect()
