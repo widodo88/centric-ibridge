e9fd9db127c949ea37b4ad735d921c183b3d4ba6
muhammad.aqshol <muhammad.aqshol@usg.co.id>
2022-02-04 13:11:20 +0700

    Adding License Stomptransport.py

--- core/transport/stomptransport.py
+++ core/transport/stomptransport.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019 Busana Apparel Group. All rights reserved.
+# Copyright (c) 2022 Busana Apparel Group. All rights reserved.
 #
 # This product and it's source code is protected by patents, copyright laws and
 # international copyright treaties, as well as other intellectual property
@@ -9,6 +9,9 @@
 # as well as the documentation shall not be copied, modified or redistributed
 # without permission, explicit or implied, of the author.
 #
+# This module is part of Centric PLM Integration Bridge and is released under
+# the Apache-2.0 License: https://www.apache.org/licenses/LICENSE-2.0
+
 from core.transhandler import TransportHandler
 from stompest.config import StompConfig
 from stompest.protocol.spec import StompSpec
