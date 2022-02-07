abfb898f7135219f45a6710fa2e934e31a747936
Jaimy Azle <jaimy@usg.co.id>
2022-02-07 18:38:51 +0700

    add base JWT Auth login service

---
+++ common/restapikey.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Busana Apparel Group. All rights reserved.
+#
+# This product and it's source code is protected by patents, copyright laws and
+# international copyright treaties, as well as other intellectual property
+# laws and treaties. The product is licensed, not sold.
+#
+# The source code and sample programs in this package or parts hereof
+# as well as the documentation shall not be copied, modified or redistributed
+# without permission, explicit or implied, of the author.
+#
+# This module is part of Centric PLM Integration Bridge and is released under
+# the Apache-2.0 License: https://www.apache.org/licenses/LICENSE-2.0
+
+from pydantic import BaseModel
+from fastapi_jwt_auth import AuthJWT
+
+
+class JWTSettings(BaseModel):
+
+    authjwt_secret_key = "qwelmckjadlJK12KL-BUSANAGROUP"
+
+
+@AuthJWT.load_config
+def get_config():
+    return JWTSettings()
